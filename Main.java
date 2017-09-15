120ebc06988a8dab3b8319ea9e4a8490b16e749e
mekalaswathi <31917214+mekalaswathi@users.noreply.github.com>
2017-09-15 22:24:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
